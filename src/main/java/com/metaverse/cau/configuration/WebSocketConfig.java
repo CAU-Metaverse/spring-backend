20a4913f48435aadcc86415723ae0ae67da1d643
Muang <tnfbxks0224@naver.com>
2023-11-06 00:06:28 +0900

    수강신청 경쟁 - 아직수정필요

--- src/main/java/com/metaverse/cau/configuration/WebSocketConfig.java
+++ src/main/java/com/metaverse/cau/configuration/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.metaverse.cau.configuration;
 
-import com.metaverse.cau.interfaces.MyWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.metaverse.cau.interfaces.MyWebSocketHandler;
+import com.metaverse.cau.interfaces.SugangWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
@@ -14,5 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new MyWebSocketHandler(), "/my-websocket")
                 .setAllowedOrigins("*"); // WebSocket 핸들러를 "/my-websocket" 경로로 등록
+        registry.addHandler(new SugangWebSocketHandler(), "/sugang-websocket")
+        		.setAllowedOrigins("*");
     }
 }
